3ea471e476ad6e378acb1c9dc2df47b40c483b10
piyasnhu31 <piyanshukale2005@gmail.com>
2025-05-05 10:50:17 +0530

    change

--- bitstuffing.c
+++ bitstuffing.c
@@ -2,7 +2,7 @@
 
 void main()
 {
-    int a[30],b[30],i=0,j=0,k,n,count;
+    int a[30],b[30],i,j=0,k,n,count=0;
     printf("Enter frame size: ");
     scanf("%d",&n);
     printf("Enter frame data in 0 and 1 formate: ");
@@ -11,36 +11,28 @@ void main()
         scanf("%d",&a[i]);
     }
 
-    while(i<n)
+    for(i=0;i<n;i++)
     {
-        if(a[i]==1)
+        b[j]=a[i];
+        if(a[i]== 1)
         {
-            count=1;
-            b[j]=a[i];
-
-            for(k=i+1;a[k]==1&&k<n&&count<5;k++)
-            {
-                j++;
-                b[j]=a[k];
-                count++;
-            if(count == 5)
+            count++;
+            if (count == 5)
             {
                 j++;
                 b[j]=0;
-            }
-            i=k;
+                count=0;
             }
         }
         else
         {
-            b[j]=a[i];
-            i++;
-            j++;
+            count =0;
         }
+        j++;
     }
 
     printf("After bit stuffing: ");
-    for(i=0;i<j;i++)
+    for(i=0;i<n;i++)
     {
         printf("%d",b[i]);
     }
